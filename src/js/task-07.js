9372768b32ec1d8e5d033f49a02d4100cc00a306
Oleg2018a <126179919+Oleg2018a@users.noreply.github.com>
2023-07-03 15:51:40 +0300

    Видалені

--- src/js/task-07.js
+++
@@ -1,8 +0,0 @@
-const inputValueSize = document.querySelector("#font-size-control");
-const textValue = document.querySelector("#text");
-
-inputValueSize.addEventListener("input", onChangeSize);
-console.dir(inputValueSize);
-function onChangeSize() {
-  textValue.style.fontSize = inputValueSize.value + "px";
-}
